7eed643f1790221a38bc11c389b4f3d3acf12ce1
Artem <morcik155@gmail.com>
2022-02-19 19:23:27 +0300

    update

--- app/src/main/java/com/example/CityOfLightsFLL/MainActivity.java
+++ app/src/main/java/com/example/CityOfLightsFLL/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.CityOfLightsFLL;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button about_us = findViewById(R.id.about_us_main);
+        about_us.setBackgroundResource(R.drawable.button_shape);
     }
 }
